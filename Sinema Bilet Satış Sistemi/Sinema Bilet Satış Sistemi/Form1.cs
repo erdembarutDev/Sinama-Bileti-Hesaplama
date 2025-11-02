14be43e54ddcbdb8ad6a42f3521a66e9a5bfd67b
Erdem Barut <150465502+erdembarutDev@users.noreply.github.com>
2025-11-02 22:59:20 +0300

    Update Form1.cs

--- Sinema Bilet Satış Sistemi/Sinema Bilet Satış Sistemi/Form1.cs
+++ Sinema Bilet Satış Sistemi/Sinema Bilet Satış Sistemi/Form1.cs
@@ -17,10 +17,6 @@ namespace Sinema_Bilet_Satış_Sistemi
             InitializeComponent();
         }
 
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
